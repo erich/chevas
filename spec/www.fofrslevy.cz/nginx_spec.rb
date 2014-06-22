ea83da6553abeee54ebfbcdc44bc7a0f49663d15
Erich Kaderka <erich.kaderka@gmail.com>
2014-06-22 14:20:01 +0200

    Improve nginx spec

--- spec/www.fofrslevy.cz/nginx_spec.rb
+++ spec/www.fofrslevy.cz/nginx_spec.rb
@@ -6,4 +6,14 @@ describe 'Sheldon 6artisans webserver' do
     expect(package 'nginx').to be_installed
   end
 
+  it 'is running' do
+    expect(service 'httpd').to be_enabled
+    expect(service 'httpd').to be_running
+  end
+
+  xit 'responds to request with response from rails app' do
+    #think about adding domains to host file
+    expect(command 'curl http://www.fofrslevy.cz').to return_stdout(/.*<title>Fofrslevy.cz<\/title>.*/)
+  end
+
 end
